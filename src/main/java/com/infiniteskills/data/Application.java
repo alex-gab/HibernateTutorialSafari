78b8c31bdde570870dc2cdb98efc87d5d458d3b8
alex-gab <dragoi.alex@gmail.com>
2016-11-12 19:01:12 +0200

    [#1]: OneToMany

--- src/main/java/com/infiniteskills/data/Application.java
+++ src/main/java/com/infiniteskills/data/Application.java
@@ -1,44 +1,77 @@
 package com.infiniteskills.data;
 
-import com.infiniteskills.data.entities.Credential;
-import com.infiniteskills.data.entities.User;
+import com.infiniteskills.data.entities.Account;
+import com.infiniteskills.data.entities.Transaction;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 
 public class Application {
 
     public static void main(String[] args) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
-            User user = new User();
-            user.setFirstName("Kevin");
-            user.setLastName("Bowersox");
-            user.setAge(20);
-            user.setBirthDate(new Date());
-            user.setCreatedBy("Kevin Bowersox");
-            user.setCreatedDate(new Date());
-            user.setEmailAddress("kevin.bowersox@navy.mil");
-            user.setLastUpdatedDate(new Date());
-            user.setLastUpdatedBy("Kevin Bowersox");
+        try {
+            org.hibernate.Transaction transaction = session.beginTransaction();
 
-            Credential credential = new Credential();
-            credential.setPassword("kevinspassword");
-            credential.setUsername("kmb385");
-            credential.setUser(user);
+            Account account = createNewAccount();
+            account.addTransaction(createNewBeltPurchase());
+            account.addTransaction(createShoePurchase());
+            session.save(account);
 
-            session.save(credential);
             transaction.commit();
 
-            final User dbUser = session.get(User.class, credential.getUser().getUserId());
-            System.out.println(dbUser.getFirstName());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            session.close();
             HibernateUtil.getSessionFactory().close();
         }
+    }
+
+    private static Transaction createNewBeltPurchase() {
+        Transaction beltPurchase = new Transaction();
+        beltPurchase.setTitle("Dress Belt");
+        beltPurchase.setAmount(new BigDecimal("50.00"));
+        beltPurchase.setClosingBalance(new BigDecimal("0.00"));
+        beltPurchase.setCreatedBy("Kevin Bowersox");
+        beltPurchase.setCreatedDate(new Date());
+        beltPurchase.setInitialBalance(new BigDecimal("0.00"));
+        beltPurchase.setLastUpdatedBy("Kevin Bowersox");
+        beltPurchase.setLastUpdatedDate(new Date());
+        beltPurchase.setNotes("New Dress Belt");
+        beltPurchase.setTransactionType("Debit");
+        return beltPurchase;
+    }
+
+    private static Transaction createShoePurchase() {
+        Transaction shoePurchase = new Transaction();
+        shoePurchase.setTitle("Work Shoes");
+        shoePurchase.setAmount(new BigDecimal("100.00"));
+        shoePurchase.setClosingBalance(new BigDecimal("0.00"));
+        shoePurchase.setCreatedBy("Kevin Bowersox");
+        shoePurchase.setCreatedDate(new Date());
+        shoePurchase.setInitialBalance(new BigDecimal("0.00"));
+        shoePurchase.setLastUpdatedBy("Kevin Bowersox");
+        shoePurchase.setLastUpdatedDate(new Date());
+        shoePurchase.setNotes("Nice Pair of Shoes");
+        shoePurchase.setTransactionType("Debit");
+        return shoePurchase;
+    }
+
+    private static Account createNewAccount() {
+        Account account = new Account();
+        account.setCloseDate(new Date());
+        account.setOpenDate(new Date());
+        account.setCreatedBy("Kevin Bowersox");
+        account.setInitialBalance(new BigDecimal("50.00"));
+        account.setName("Savings Account");
+        account.setCurrentBalance(new BigDecimal("100.00"));
+        account.setLastUpdatedBy("Kevin Bowersox");
+        account.setLastUpdatedDate(new Date());
+        account.setCreatedDate(new Date());
+        return account;
     }
 }
