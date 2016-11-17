3e764acf554ce9ad812d462624943f63c2a5c0ab
alex-gab <dragoi.alex@gmail.com>
2016-11-17 08:43:03 +0200

    [#1]: advanced

--- hibernate/src/main/java/com/infiniteskills/data/HibernateUtil.java
+++ hibernate/src/main/java/com/infiniteskills/data/HibernateUtil.java
@@ -5,6 +5,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+@SuppressWarnings("Duplicates")
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
